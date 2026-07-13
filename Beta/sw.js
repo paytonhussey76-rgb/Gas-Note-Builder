58a81068e012c8e709d06736ec7b2cf9b0913736
paytonhussey76-rgb <paytonhussey76@gmail.com>
2026-07-13 14:17:03 -0700

    Add files via upload
    Made beta

---
+++ Beta/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'gas-locate-notes-v2';
+const CORE_FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first: always try to get the latest version when online.
+// Only fall back to the cached copy if there's no signal.
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request))
+  );
+});
